3795d003b4fb141a4c952a11086c906fd642ba82
vagrant <vagrant@homestead>
2017-09-04 19:49:40 +0000

    Cambio al home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Estás registrado!
                 </div>
             </div>
         </div>
