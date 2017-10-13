6562bd4049b5ea5556213281ff12a0f6044d34aa
mmarin49 <manelma@gmail.com>
2017-10-13 18:33:38 +0200

    Revert "cambios_israel"
    This reverts commit 09d14594eeaa95c92defb313367ceb27425711dc.

--- resources/views/partials/admin-nav.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="col-md-12">
-    {{link_to_action('Admin\PizzaController@index', 'Pizzas',[],[])}} |
-    {{link_to_action('Admin\UserController@index', 'Usuarios',[],[])}} |
-    {{link_to_action('Admin\IngredientController@index', 'Ingredientes',[],[])}} |
-    {{link_to_action('AdminController@index', 'Administración',[],[])}}
-
-</div>
-
-<hr />
